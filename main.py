a85fb81032715ddffff6298fe7e368ff93cacced
ZavateVlad <95172779+ZavateVlad@users.noreply.github.com>
2023-11-06 15:02:21 +0000

    Cleaned code

--- main.py
+++ main.py
@@ -1,44 +1,8 @@
-############### Blackjack Project #####################
-
-#Difficulty Normal 😎: Use all Hints below to complete the project.
-#Difficulty Hard 🤔: Use only Hints 1, 2, 3 to complete the project.
-#Difficulty Extra Hard 😭: Only use Hints 1 & 2 to complete the project.
-#Difficulty Expert 🤯: Only use Hint 1 to complete the project.
-
-############### Our Blackjack House Rules #####################
-
-## The deck is unlimited in size. 
-## There are no jokers. 
-## The Jack/Queen/King all count as 10.
-## The the Ace can count as 11 or 1.
-## Use the following list as the deck of cards:
-## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-## The cards in the list have equal probability of being drawn.
-## Cards are not removed from the deck as they are drawn.
-## The computer is the dealer.
-
-##################### Hints #####################
-
-#Hint 1: Go to this website and try out the Blackjack game: 
-#   https://games.washingtonpost.com/games/blackjack/
-#Then try out the completed Blackjack project here: 
-#   http://blackjack-final.appbrewery.repl.run
 
 import random
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 another_card = True
-
-# # def user_cards():
-# #   user = random.sample(cards, 2)
-# #   print(user)
-# #   user_choice = input("Type 'y' to get another card, type 'n' to pass: ")
-# #   if sum(user) > 21:
-# #     print('Bust')
-# #   if user_choice == 'y':
-# #     user.append(random.choice(cards))
-# #     print(user)
-# #   else:
 
 user = random.sample(cards, 2)
 dealer = random.sample(cards, 1)
@@ -54,28 +18,8 @@ def show_cards(show):
   if show == True:
     print(f"Dealer's final hand: {dealer}, final score: {sum(dealer)}")
 
-#def compare(player1,player2):
-  
-
-
-# # def usr():
-# #   if sum(user) < 21:
-# #     add_card(user)
-
-# # def dealr():
-# #   if sum(dealer) < 17:
-# #     add_card(dealer)
-    
-# def final():
-#   print(f"Dealer's final hand: {dealer}, final score: {sum(dealer)}")
-
-# def deal():
-#   if sum(add_card(dealer)) < 17:
-#     add_card(dealer)
 
 def compare(player1,player2):
-  #player1 = sum(player1)
-  #player2 = sum(player2)
   if 1 in player1 or 1 in player2:
     if sum(player1) < 21:
       player1.remove(11)
@@ -126,14 +70,6 @@ def game():
 
 game()
 
-
-# restart = True
-# while restart:
-#   starting = input("Would you like to play a game of blackjack?: Type 'y' if yes, 'n' for no: ")
-#   if starting == 'y':
-#     game()
-#   else:
-#     restart = False
   
 
 
